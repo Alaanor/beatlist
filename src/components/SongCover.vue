e225be095f7b6cf87d7e07f5393c53fe0bd66a30
Maxime Bonvin <max.bonvin@gmail.com>
2019-09-21 18:59:54 +0200

    Placeholder for invalid/404 src SongCover

--- src/components/SongCover.vue
+++ src/components/SongCover.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-img v-if="imageData.length > 0" :src="imageData" aspect-ratio="1" contain></v-img>
+  <v-img v-if="imageData.length > 0" :src="imageData" aspect-ratio="1" contain>
+    <template v-slot:placeholder>
+      <v-row class="fill-height ma-0" align="center" justify="center">
+        <v-icon large color="grey">mdi-cancel</v-icon>
+      </v-row>
+    </template>
+  </v-img>
   <v-progress-circular v-else indeterminate></v-progress-circular>
 </template>
 
