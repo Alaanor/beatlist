dbef8289d5a22bfb7caaa082929682876a5af914
Maxime Bonvin <max.bonvin@gmail.com>
2019-11-16 19:28:47 +0100

    Escape all shitty character on the generated folder name when downloading a new beatmap

--- src/lib/BeatSaber.ts
+++ src/lib/BeatSaber.ts
@@ -86,7 +86,10 @@ export default class BeatSaber {
 
   public async CreateBeatMapFolder(beatmap: ISongOnline) {
     const songFolder = path.join(this.installationPath, BEAT_SABER_CUSTOM_LEVEL);
-    const newFolderName = `${beatmap.key} (${beatmap.metadata.songName} - ${beatmap.metadata.levelAuthorName})`;
+    const newFolderName = `${beatmap.key} (${beatmap.metadata.songName} - ${beatmap.metadata.levelAuthorName})`
+      .replace(/[|&;$%@"<>()+,]/g, '')
+      .replace('\\', '_')
+      .replace('/', '_');
     const finalPath = path.join(songFolder, newFolderName);
 
     if (!(await exists(finalPath))) {
