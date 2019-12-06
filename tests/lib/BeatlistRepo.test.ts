5cee84753b31803387d67cae06def3c6867f5820
Maxime Bonvin <max.bonvin@gmail.com>
2019-12-06 16:47:13 +0100

    Added coverage, made test to get 100% coverage, refactored folder name for consistency

--- tests/lib/BeatlistRepo.test.ts
+++ tests/lib/BeatlistRepo.test.ts
@@ -1,13 +1,41 @@
 import BeatlistRepo from '../../src/libraries/net/github/BeatlistRepo';
+import AxiosCachedFactory from '../../src/libraries/net/AxiosCachedFactory';
+
+jest.mock('../../src/libraries/net/AxiosCachedFactory');
 
 describe('beatlist repo', () => {
-  it('changelog content fetcher', async () => {
+  it('should get the changelog content', async () => {
     expect.assertions(2);
+
+    const mockGetAxios = jest.fn();
+    mockGetAxios.mockReturnValue({
+      get: async () => ({ data: '# Changelog' }),
+    });
+
+    AxiosCachedFactory.getAxios = mockGetAxios;
 
     const repo = new BeatlistRepo();
     const data = await repo.GetChangelogContent();
 
     expect(data).toBeDefined();
     expect(data).toMatch(/^# Changelog/);
+  });
+
+  it('shouldn\'t get the changelog content, 404', async () => {
+    expect.assertions(1);
+
+    const mockGetAxios = jest.fn();
+    mockGetAxios.mockImplementation(() => ({
+      get: async () => {
+        throw new Error('no internet for you human');
+      },
+    }));
+
+    AxiosCachedFactory.getAxios = mockGetAxios;
+
+    const repo = new BeatlistRepo();
+    const data = await repo.GetChangelogContent();
+
+    expect(data).toBeUndefined();
   });
 });
