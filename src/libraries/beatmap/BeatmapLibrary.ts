46a428724706d23c31c1b75cb9f3938786914ce3
Maxime Bonvin <max.bonvin@gmail.com>
2019-12-11 22:53:26 +0100

    Ignored some place for the code coverage, make no sense to cover there.

--- src/libraries/beatmap/BeatmapLibrary.ts
+++ src/libraries/beatmap/BeatmapLibrary.ts
@@ -2,10 +2,12 @@ import store from '../../plugins/store';
 import { BeatmapLocal } from './BeatmapLocal';
 
 export default class BeatmapLibrary {
+  /* istanbul ignore next */
   public static GetAllMaps(): BeatmapLocal[] {
     return store.getters['beatmap/beatmaps'] as BeatmapLocal[];
   }
 
+  /* istanbul ignore next */
   public static UpdateAllMaps(beatmaps: BeatmapLocal[]) {
     store.commit('beatmap/SET_BEATMAPS', beatmaps);
   }
--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VuexPersistence from 'vuex-persist';
