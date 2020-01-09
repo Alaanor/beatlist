1a76321f50c5bcc293e0b4da4fb7a9a241e30ec7
Maxime Bonvin <max.bonvin@gmail.com>
2020-01-09 16:32:26 +0100

    Custom/Better footer for the table

--- src/components/beatmap/table/BeatmapsTable.vue
+++ src/components/beatmap/table/BeatmapsTable.vue
@@ -19,9 +19,63 @@
     <v-data-table
       :headers="getHeaders()"
       :items="beatmaps"
+      :options="{...options, itemsPerPage}"
+      hide-default-footer
       fixed-header
       dense
     >
+      <template #footer="{ props: { pagination } }">
+        <v-divider/>
+        <div class="d-flex justify-end align-center small-font py-1">
+          <span>Rows per page:</span>
+          <v-menu>
+            <template #activator="{ on }">
+              <v-btn
+                small
+                text
+                outlined
+                class="mx-3"
+                v-on="on"
+              >
+                {{ itemsPerPage | withAll }}
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="item in footerOptions.itemPerPageList"
+                :key="item"
+                @click="itemsPerPage = item"
+              >
+                <v-list-item-title>{{ item | withAll }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <span class="px-2">
+            {{ `${pagination.pageStart+1}-${pagination.pageStop} of ${pagination.itemsLength}` }}
+          </span>
+          <v-btn
+            icon
+            small
+            class="ml-2 mr-1"
+            @click="options.page--"
+          >
+            <v-icon small>
+              chevron_left
+            </v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            small
+            class="ml-1 mr-5"
+            @click="options.page++"
+          >
+            <v-icon small>
+              chevron_right
+            </v-icon>
+          </v-btn>
+        </div>
+      </template>
+
       <template #item.cover="{ item }">
         <BeatmapCover
           :beatmap="item.local || item.data"
@@ -95,6 +149,11 @@ import { BeatmapsTableDataUnit } from '@/components/beatmap/table/BeatmapsTableD
 export default Vue.extend({
   name: 'BeatmapLocal',
   components: { BeatmapCover, DifficultiesChips, Tooltip },
+  filters: {
+    withAll(item: number): string {
+      return item === -1 ? 'All' : item.toString();
+    },
+  },
   props: {
     beatmaps: { type: Array as PropType<BeatmapsTableDataUnit[]>, required: true },
   },
@@ -167,9 +226,16 @@ export default Vue.extend({
         sortable: true,
       },
     ],
+    footerOptions: {
+      itemPerPageList: [5, 10, 15, 20, 25, 50, 100, -1],
+    },
+    options: {
+      page: 1,
+    },
   }),
   computed: {
     shownColumn: sync<string[]>('settings/beatmapsTable@shownColumn'),
+    itemsPerPage: sync<number>('settings/beatmapsTable@itemsPerPage'),
   },
   methods: {
     getHeaders() {
@@ -180,5 +246,7 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-
+  .small-font {
+    font-size: 12px;
+  }
 </style>
