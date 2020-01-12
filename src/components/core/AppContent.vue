6811f9438e43c9641cd0f6c66d045005478327e6
Maxime Bonvin <max.bonvin@gmail.com>
2020-01-12 15:28:40 +0100

    Remove app content empty padding

--- src/components/core/AppContent.vue
+++ src/components/core/AppContent.vue
@@ -2,45 +2,33 @@
   <v-content>
     <v-container fluid>
       <v-row v-if="isReady">
-        <v-col
-          lg="10"
-          cols="12"
-          offset-lg="1"
+        <transition
+          name="slide-x-transition"
+          mode="out-in"
         >
-          <transition
-            name="slide-x-transition"
-            mode="out-in"
-          >
-            <router-view />
-          </transition>
-        </v-col>
+          <router-view />
+        </transition>
       </v-row>
       <v-row
         v-else
       >
-        <v-col
-          lg="10"
-          cols="12"
-          offset-lg="1"
-        >
-          <div class="d-flex justify-center align-center flex-column fill-height">
-            <v-progress-circular
-              :size="75"
-              :width="6"
-              color="grey"
-              indeterminate
-            />
-            <span class="pt-3 grey--text">Loading content ...</span>
-            <transition name="slide-x-transition">
-              <span
-                v-if="twoSecond"
-                class="pt warning--text caption"
-              >
-                If you're reading this message, there's probably a problem
-              </span>
-            </transition>
-          </div>
-        </v-col>
+        <div class="d-flex justify-center align-center flex-column fill-height">
+          <v-progress-circular
+            :size="75"
+            :width="6"
+            color="grey"
+            indeterminate
+          />
+          <span class="pt-3 grey--text">Loading content ...</span>
+          <transition name="slide-x-transition">
+            <span
+              v-if="twoSecond"
+              class="pt warning--text caption"
+            >
+              If you're reading this message, there's probably a problem
+            </span>
+          </transition>
+        </div>
       </v-row>
     </v-container>
   </v-content>
