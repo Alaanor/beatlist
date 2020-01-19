03dc59c6eb54c96f7c0e60275a9c65e7074cb602
Maxime Bonvin <max.bonvin@gmail.com>
2020-01-19 03:41:23 +0100

    Wrote BeatmapSummaryCharacteristic

--- src/components/beatmap/info/BeatmapSummaryCharacteristic.vue
+++ src/components/beatmap/info/BeatmapSummaryCharacteristic.vue
@@ -1,16 +1,39 @@
 <template>
-  <v-container/>
+  <v-card flat>
+    <v-tabs v-model="tab">
+      <v-tab
+        v-for="characteristic in beatmap.metadata.characteristics"
+        :key="characteristic.name"
+      >
+        {{ characteristic.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="characteristic in beatmap.metadata.characteristics"
+        :key="characteristic.name"
+      >
+        <BeatmapSummaryDifficultiesTabs :characteristic="characteristic"/>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue';
 import { BeatsaverBeatmap } from '@/libraries/net/beatsaver/BeatsaverBeatmap';
+import BeatmapSummaryDifficultiesTabs from '@/components/beatmap/info/characteristic/BeatmapSummaryDifficultiesTabs.vue';
 
 export default Vue.extend({
   name: 'BeatmapSummaryCharacteristic',
+  components: { BeatmapSummaryDifficultiesTabs },
   props: {
     beatmap: { type: Object as PropType<BeatsaverBeatmap>, required: true },
   },
+  data: () => ({
+    tab: null,
+  }),
 });
 </script>
 
---
+++ src/components/beatmap/info/characteristic/BeatmapSummaryDifficulty.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-row align="center">
+    <v-col cols="auto">
+      <table>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            Duration
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.duration }}
+          </td>
+        </tr>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            length
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.length }}
+          </td>
+        </tr>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            Bombs
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.bombs }}
+          </td>
+        </tr>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            Notes
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.notes }}
+          </td>
+        </tr>
+      </table>
+    </v-col>
+    <v-col cols="auto">
+      <table>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            Obstacles
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.obstacles }}
+          </td>
+        </tr>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            njs
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.njs }}
+          </td>
+        </tr>
+        <tr>
+          <td
+            class="body-2 pr-2"
+            align="right"
+          >
+            njs offset
+          </td>
+          <td class="body-1 pl-2">
+            {{ difficulty.njsOffset }}
+          </td>
+        </tr>
+      </table>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { Difficulty } from '@/libraries/net/beatsaver/BeatsaverBeatmap';
+
+export default Vue.extend({
+  name: 'BeatmapSummaryDifficulty',
+  props: {
+    difficulty: { type: Object as PropType<Difficulty>, required: true },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
