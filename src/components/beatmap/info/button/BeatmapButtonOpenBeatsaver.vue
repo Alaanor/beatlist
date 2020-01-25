2898f0a745b93f9e7b6b23dfa1a3a6d33a1c9847
Maxime Bonvin <max.bonvin@gmail.com>
2020-01-25 20:34:35 +0100

    Some beatmap info actions button

---
+++ src/components/beatmap/info/button/BeatmapButtonOpenBeatsaver.vue
@@ -0,0 +1,35 @@
+<template>
+  <Tooltip text="Open on beatsaver">
+    <v-btn
+      icon
+      @click="openBeatsaver"
+    >
+      <v-icon>open_in_new</v-icon>
+    </v-btn>
+  </Tooltip>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { shell } from 'electron';
+import { BeatsaverBeatmap } from '@/libraries/net/beatsaver/BeatsaverBeatmap';
+import Tooltip from '@/components/helper/Tooltip.vue';
+import BeatsaverUtilities from '@/libraries/net/beatsaver/BeatsaverUtilities';
+
+export default Vue.extend({
+  name: 'BeatmapButtonOpenBeatsaver',
+  components: { Tooltip },
+  props: {
+    beatmap: { type: Object as PropType<BeatsaverBeatmap>, required: true },
+  },
+  methods: {
+    openBeatsaver(): void {
+      shell.openExternal(BeatsaverUtilities.GetPageUrl(this.beatmap));
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
