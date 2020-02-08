de7454287dfd5c8b0dee78fe2fe91803b1c8882e
Maxime Bonvin <max.bonvin@gmail.com>
2020-02-08 18:20:16 +0100

    Wrote the output message for playlist scanner result

--- src/libraries/scanner/playlist/PlaylistScannerResult.ts
+++ src/libraries/scanner/playlist/PlaylistScannerResult.ts
@@ -8,7 +8,27 @@ export default class PlaylistScannerResult implements ScannerResultInterface {
 
   public keptItems: number = 0;
 
+  public updatedItems: number = 0;
+
   public toString(): string {
-    return ``; // @TODO
+    const newItemsStr = this.newItems.length > 0
+      ? `${this.newItems.length} playlist${this.newItems.length > 1 ? 's' : ''} has been added`
+      : '';
+
+    const removedItemsStr = this.removedItems > 0
+      ? `${this.removedItems} playlist${this.removedItems > 1 ? 's' : ''} has been removed`
+      : '';
+
+    const keptItemsStr = this.keptItems > 0
+      ? `${this.keptItems} playlist${this.keptItems > 1 ? 's' : ''} has been kept`
+      : '';
+
+    const updatedItemsStr = this.updatedItems > 0
+      ? `${this.updatedItems} playlist${this.updatedItems > 1 ? 's' : ''} has been update`
+      : '';
+
+    return [newItemsStr, removedItemsStr, keptItemsStr, updatedItemsStr]
+      .filter((s: string) => s !== '')
+      .join(' and ');
   }
 }
