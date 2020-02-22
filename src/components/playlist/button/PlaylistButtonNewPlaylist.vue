5f9a93905b5cf0dfccc2327a1fba2bc8e21558ee
Maxime Bonvin <max.bonvin@gmail.com>
2020-02-22 18:15:37 +0100

    loading state for the add new playlist button

--- src/components/playlist/button/PlaylistButtonNewPlaylist.vue
+++ src/components/playlist/button/PlaylistButtonNewPlaylist.vue
@@ -2,6 +2,7 @@
   <Tooltip text="New playlist">
     <v-btn
       icon
+      :loading="loading"
       @click="MakeNewPlaylist"
     >
       <v-icon>add</v-icon>
@@ -17,9 +18,16 @@ import PlaylistInstaller from '@/libraries/os/beatSaber/installer/PlaylistInstal
 export default Vue.extend({
   name: 'PlaylistButtonNewPlaylist',
   components: { Tooltip },
+  data: () => ({
+    loading: false,
+  }),
   methods: {
     MakeNewPlaylist() {
-      PlaylistInstaller.InstallNewEmpty();
+      this.loading = true;
+      PlaylistInstaller.InstallNewEmpty()
+        .then(() => {
+          this.loading = false;
+        });
     },
   },
 });
