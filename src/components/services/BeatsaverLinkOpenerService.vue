af84ba85a8ca7c83b8a3cb763aaa901bff835a7c
Maxime Bonvin <max.bonvin@gmail.com>
2020-05-13 16:44:06 +0200

    Install the map if the option is enable on oneclick and if not downloaded already

--- src/components/services/BeatsaverLinkOpenerService.vue
+++ src/components/services/BeatsaverLinkOpenerService.vue
@@ -14,6 +14,10 @@ import { BeatsaverKeyType } from "@/libraries/beatmap/repo/BeatsaverKeyType";
 import NotificationService, {
   NOTIFICATION_ICON_FAILED,
 } from "@/libraries/notification/NotificationService";
+import BeatmapInstaller from "@/libraries/os/beatSaber/installer/BeatmapInstaller";
+import { BeatsaverBeatmap } from "@/libraries/net/beatsaver/BeatsaverBeatmap";
+import store from "@/plugins/store";
+import BeatmapLibrary from "@/libraries/beatmap/BeatmapLibrary";
 
 export default Vue.extend({
   name: "BeatsaverLinkOpenerService",
@@ -21,28 +25,49 @@ export default Vue.extend({
     ipcRenderer.on(
       OPEN_BEATSAVER_LINK,
       async (event: IpcRendererEvent, key: string) => {
-        const beatsaverItem = await BeatsaverCacheManager.forceGetCacheBeatmap({
-          type: BeatsaverKeyType.Key,
-          value: key,
-        });
-
-        if (beatsaverItem.beatmap) {
-          await this.$router.push({
-            name: "beatmaps-online-unit",
-            params: { hash: beatsaverItem.beatmap.hash },
-          });
-        } else {
-          NotificationService.NotifyMessage(
-            `Failed to find the key ${key} on beatsaver.`,
-            "warning",
-            NOTIFICATION_ICON_FAILED,
-            2500
-          );
-        }
+        this.handleOneClick(key);
       }
     );
 
     ipcRenderer.send(ON_BEATSAVER_LINK_OPENER_COMPONENT_READY);
+  },
+  methods: {
+    async handleOneClick(key: string): Promise<void> {
+      const beatsaverItem = await BeatsaverCacheManager.forceGetCacheBeatmap({
+        type: BeatsaverKeyType.Key,
+        value: key,
+      });
+
+      if (beatsaverItem.beatmap) {
+        await this.checkForInstall(beatsaverItem.beatmap);
+
+        await this.$router.push({
+          name: "beatmaps-online-unit",
+          params: { hash: beatsaverItem.beatmap.hash },
+        });
+      } else {
+        NotificationService.NotifyMessage(
+          `Failed to find the key ${key} on beatsaver.`,
+          "warning",
+          NOTIFICATION_ICON_FAILED,
+          2500
+        );
+      }
+    },
+    async checkForInstall(beatsaverBeatmap: BeatsaverBeatmap): Promise<void> {
+      if (!store.getters["settings/oneClick"].downloadOnClick) {
+        return;
+      }
+
+      if (BeatmapLibrary.HasBeatmap(beatsaverBeatmap)) {
+        NotificationService.NotifyMessage("This map is already installed.");
+        return;
+      }
+
+      BeatmapInstaller.Install(beatsaverBeatmap, (operation) =>
+        NotificationService.NotifyBeatmapDownload(operation.result)
+      );
+    },
   },
 });
 </script>
