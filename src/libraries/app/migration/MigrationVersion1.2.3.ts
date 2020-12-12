63e9ad67f2adffc2629fb197249448974721c3e3
Maxime Bonvin <max.bonvin@gmail.com>
2020-12-12 21:27:46 +0100

    Migration removing all certain invalid cache to retry them since it was probably an invalid due to rate limit which is now fixed

--- src/libraries/app/migration/MigrationVersion1.2.3.ts
+++ src/libraries/app/migration/MigrationVersion1.2.3.ts
@@ -1,7 +1,12 @@
 import store from "@/plugins/store";
 import PlaylistLibrary from "@/libraries/playlist/PlaylistLibrary";
+import BeatsaverCachedLibrary from "@/libraries/beatmap/repo/BeatsaverCachedLibrary";
+import {
+  BeatsaverItemInvalid,
+  BeatsaverItemLoadError,
+} from "@/libraries/beatmap/repo/BeatsaverItem";
 
-export default () => {
+function RemoveAllCoverFromPlaylistCache() {
   const allPlaylist = PlaylistLibrary.GetAllPlaylists();
   const allPlaylistWithoutCover = allPlaylist.map((playlist) => {
     const copy = { ...playlist };
@@ -10,4 +15,24 @@ export default () => {
   });
 
   store.commit("playlist/SET_PLAYLISTS", allPlaylistWithoutCover);
+}
+
+function RemoveAllInvalidBeatmapFromInvalidCache() {
+  const allInvalidBeatmap = BeatsaverCachedLibrary.GetAllInvalid();
+
+  Array.from(allInvalidBeatmap.values()).forEach(
+    (item: BeatsaverItemInvalid) => {
+      if (
+        item.loadState.errorType ===
+        BeatsaverItemLoadError.BeatsaverServerNotAvailable
+      ) {
+        BeatsaverCachedLibrary.RemoveInvalid(item.loadState.attemptedSource);
+      }
+    }
+  );
+}
+
+export default () => {
+  RemoveAllCoverFromPlaylistCache();
+  RemoveAllInvalidBeatmapFromInvalidCache();
 };
