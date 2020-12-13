89859ae10c969381435f1921f5ed002f9ef55978
Maxime Bonvin <max.bonvin@gmail.com>
2020-12-13 20:56:03 +0100

    typo in BeatSaberPlus

--- src/libraries/net/beatsaver/BeatsaverServerUrl.ts
+++ src/libraries/net/beatsaver/BeatsaverServerUrl.ts
@@ -1,6 +1,6 @@
 enum BeatsaverServerUrl {
   Beatsaver = "https://beatsaver.com/api", // default, original
-  BeatsaberPlus = "https://maps.beatsaberplus.com/api/", // mirror
+  BeatSaberPlus = "https://maps.beatsaberplus.com/api/", // mirror
 }
 
 export default BeatsaverServerUrl;
